41c1b9a93820878466ab581a6ab7bdc1b42e6508
Jeremee Bornstein <jeremee.bornstein@gmail.com>
2022-03-07 14:08:00 -0500

    added schema for setup of plants including name, bionomial name, slug, acquisition date, image array with date.

---
+++ schemas/hobbies/plants.js
@@ -0,0 +1,72 @@
+export default {
+    name: 'plants',
+    title: 'Plants',
+    type: 'document',
+    fields: [
+      {
+        name: 'name',
+        title: 'Name',
+        type: 'string',
+      },
+      {
+          name: 'binomial',
+          title: 'Binomial Name',
+          type: 'string',
+
+      },
+      {
+        name: 'acquisitionDate',
+        title: 'Acquisition Date',
+        type: 'date',
+
+      },
+      {
+        name: 'slug',
+        title: 'Slug',
+        type: 'slug',
+        options: {
+          source: 'name',
+          maxLength: 96,
+        },
+      },
+      {
+        name: 'images',
+        title: 'Images',
+        type: 'array',
+        of: [{
+            name: 'image',
+            title: 'Image',
+            type: 'image',
+            fields: [{
+                name: 'imageDate',
+                title: 'Image Date',
+                type: 'date',
+                options: {
+                    isHighlighted: true // <-- make this field easily accessible
+                }
+            }
+            ]
+        }]
+    },
+      {
+        name: 'bio',
+        title: 'Bio',
+        type: 'array',
+        of: [
+          {
+            title: 'Block',
+            type: 'block',
+            styles: [{title: 'Normal', value: 'normal'}],
+            lists: [],
+          },
+        ],
+      },
+    ],
+    preview: {
+      select: {
+        title: 'name',
+        media: 'image',
+      },
+    },
+  }
+  
